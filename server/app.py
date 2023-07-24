a05669cd434cc10263a5529697e97574fba3a679
Kartikey Tewari <kartikeya30@gmail.com>
2023-07-24 18:19:39 +0530

    change hardcoded credentials to load from env file

--- server/app.py
+++ server/app.py
@@ -9,11 +9,11 @@ def get_connection():
     print ("establising connection")
     try:
         val = psycopg2.connect(
-            dbname=os.environ.get(POSTGRES_DB),
-            user=os.environ.get(POSTGRES_USER), 
-            password=os.environ.get(POSTGRES_USER), 
-            host=os.environ.get(HOST_IP),
-            port=os.environ.get(POSTGRES_PORT)
+            dbname=os.environ.get("POSTGRES_DB"),
+            user=os.environ.get("POSTGRES_USER"), 
+            password=os.environ.get("POSTGRES_PASSWORD"), 
+            host=os.environ.get("HOST_IP"),
+            port=os.environ.get("POSTGRES_PORT")
         )
         return val
     except (Exception, psycopg2.Error) as error:
