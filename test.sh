38f0f71ae3ff2296a39c1efb7bbb2761b71c5a9a
Kartikey Tewari <kartikeya30@gmail.com>
2023-08-05 01:36:34 +0530

    modify test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 curl -X POST "http://localhost:5002/identify" \
 -H "Content-type: application/json" \
--d '{"email":"sample@mail.com", "phoneNumber":"123456"}'
+-d '{"email":"series_1@sample_mail.com", "phoneNumber":"1"}'
+
+curl -X POST "http://localhost:5002/identify" \
+-H "Content-type: application/json" \
+-d '{"email":"series_2@sample_mail.com", "phoneNumber":"100"}'
