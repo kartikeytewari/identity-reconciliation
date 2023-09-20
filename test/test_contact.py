945d5fbc97fd75591fdf756913431eacb3e45cac
Kartikey Tewari <kartikeya30@gmail.com>
2023-09-21 00:34:06 +0530

    add unit test

--- test/test_contact.py
+++ test/test_contact.py
@@ -7,11 +7,15 @@ def test_series_1():
         "email": "test_1@gmail.com",
         "phoneNumber": "1"
     }
-    
+
+    print ("Sending request to server")
     val = requests.post(identity_link, json=contact_send_2_1)
+    print ("Received server response successfully")
+
     val = val.json()
     res_email = val["contact"]["emails"]
     res_phone = val["contact"]["phoneNumbers"]
+    print ("Parsed Response successfully")
 
     assert res_email == ["test_1@gmail.com"]
     assert res_phone == ["1"]
@@ -28,13 +32,15 @@ def test_series_2():
         "phoneNumber": "11"
     }
 
+    print ("Sending request to server")
     requests.post(identity_link, json=contact_send_1_1)
     val = requests.post(identity_link, json=contact_send_1_2)
+    print ("Received server response successfully")
 
     val = val.json()
     res_email = val["contact"]["emails"]
     res_phone = val["contact"]["phoneNumbers"]
-
+    print ("Parsed Response successfully")
 
     assert res_email == ["test_2@gmail.com"]
     assert res_phone == ["10", "11"]
